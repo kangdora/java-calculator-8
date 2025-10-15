ce763a6251e8402dafe41b4be422de91dbecdc64
KangDora <ioprt123@gmail.com>
2025-10-16 00:13:12 +0900

    feat(StringCalculator): 기본 구분자로 분리 후 합계 반환

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,4 +7,33 @@ public class StringCalculator {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
+
+    private int[] StringSplit(String input) {
+        String[] separators = {":", ","};
+        String regex = String.join("|", separators);
+
+        String[] tokens = input.split(regex);
+
+        int[] result = new int[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+            result[i] = Integer.parseInt(tokens[i]);
+        }
+
+        return result;
+    }
+
+    private int getSum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public void calculateString() {
+        String userInput = getUserInput();
+        int sum = getSum(StringSplit(userInput));
+
+        System.out.println("결과 : " + sum);
+    }
 }
